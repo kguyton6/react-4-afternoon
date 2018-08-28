e8564b9fafc8f53e7fcba81d1ddc399abb78bb90
kguyton6 <38778115+kguyton6@users.noreply.github.com>
2018-08-28 15:16:01 -0600

    i need to remember to to commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import './routes'
+import {Link} from 'react-router-dom'
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <nav className='nav'>
+        <nav className='nav'>      
           <div>WestSide University</div> 
           <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
+              <Link to='/' 
+              className='links'>Home</Link>
+              <Link to='/about'
+               className='links'>About</Link>
+               
           </div>
         </nav>
+        {routes}
       </div>
     )
   }
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { HashRouterProps } from 'react-router-dom'
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
-    
+  constructor(props) {
+    super(props)
+
+    this.setState = {
+      classes: []
+    }
+    this.componentDidMount(() => 
+    axios.get(`/localhost:3005/classlist?class=props`).then((req, res) => res = res.data))
+    this.setState({
+      classes: res.data
+    })
+    res.status(200)
   }
 
   render() {
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{HashRouterProps}</h1>
         <h2>ClassList:</h2>
 
       </div>
